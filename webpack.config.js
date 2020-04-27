09da3072cf92bc17a68ae5a0a66ff8ef850fd788
Mesaya <mesaya@email.com>
2020-04-27 06:18:06 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+	entry: path.resolve(__dirname, 'resources/scripts/index.js'),
+	mode: 'production',
+	output: {
+		filename: 'phobos.js',
+		path: path.resolve(__dirname, 'distribution')
+	},
+	watch: true
+}
