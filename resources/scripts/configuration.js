0be16f21be56ba559e39d3eea4d06a1832fdd4b9
Mesaya <mesaya@email.com>
2020-04-30 02:15:20 +0200

    Add blues scale

--- resources/scripts/configuration.js
+++ resources/scripts/configuration.js
@@ -7,6 +7,10 @@ export default {
 	octaves: [ -1, 10 ],
 	scales:  [
 		{
+			intervals: [ 2, 1, 0, 0, 2, 1 ],
+			name:      'Blues'
+		},
+		{
 			intervals: [ 1, 0, 1, 1, 1, 0, 1 ],
 			name:      'Dorian'
 		},
